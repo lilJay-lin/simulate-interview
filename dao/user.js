283a4f81fd0e32163d52b7b14344d56348835229
lilJay-lin <lin_xjie@foxmail.com>
2017-07-03 17:52:41 +0800

    #add 增加用户管理功能

--- dao/user.js
+++ dao/user.js
@@ -3,9 +3,18 @@
  */
 const BaseDao = require('./base')
 const UserModel = 'User'
+const _ = require('lodash')
 class UserDao extends BaseDao {
   constructor () {
     super(UserModel)
+  }
+  async pageQuery (param) {
+    let pages = await super.pageQuery(param)
+    pages.records = _.map(pages.records, (user) => {
+      user.password = ''
+      return user
+    })
+    return pages
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,101 @@ const Router = require('koa-router')
 const router = new Router()
 const UserDao = require('../dao/user')
 const userDao = new UserDao()
+const getNormalError = require('../error').getNormalError
+const _ = require('lodash')
 
+router.param('user', async (id, cxt, next) => {
+  const status = cxt.query['status'] === false || cxt.query['status'] === 'false' ? false : true
+  let users = await userDao.findPopulate({
+    queryParam: {
+      _id: userDao.caseObjectId(id),
+      status
+    },
+    populate: {
+      path: 'roles',
+      match: {status: true},
+      /*      populate: {
+       path: 'permissions',
+       match: {status: true}
+       }*/
+    }
+  })
+  if (_.isEmpty(users)) {
+    throw getNormalError('用户数据不存在，请检查')
+  }
+  cxt.user = users[0]
+  return next()
+})
+
+/*
+ * 增加
+ * */
 router.post('/', async (cxt) => {
   const body = cxt.request.body
   const user = await userDao.add(body)
   cxt.body = user
 })
 
+/*
+ * 删除
+ * */
+router.del('/:id', async (cxt) => {
+  const user = await userDao.delete({_id: userDao.caseObjectId(cxt.params.id)})
+  cxt.body = {}
+})
+
+router.del('/batch/:ids', async (cxt) => {
+  let ids = _.map((cxt.params.ids || '').split(','), (id) => {
+    return userDao.caseObjectId(id)
+  })
+  const user = await userDao.delete({_id: {$in: ids}})
+  cxt.body = {}
+})
+
+/*
+ * 修改
+ * */
+router.put('/:user', async (cxt) => {
+  const body = cxt.request.body
+  const user = await userDao.update({_id: cxt.user._id}, body)
+  cxt.body = {}
+})
+
+router.put('/batch/:ids', async (cxt) => {
+  const body = cxt.request.body
+  let ids = _.map((cxt.params.ids || '').split(','), (id) => {
+    return userDao.caseObjectId(id)
+  })
+  const user = await userDao.update({_id: {$in: ids}}, body)
+  cxt.body = {}
+})
+
+/*
+ * 搜索
+ * */
 router.get('/', async (cxt) => {
-  let list = await userDao.pageQuery({pageSize: 2, userName: {$regex: '林'}})
+  const queryParam = {}
+  const name = cxt.query['name']
+  const status = cxt.query['status'] === false || cxt.query['status'] === 'false' ? false : true
+  queryParam.status = status
+  if (name !== undefined) {
+    queryParam.name = {$regex: decodeURIComponent(name)}
+  }
+  let list = await userDao.pageQuery({queryParam})
   cxt.body = list
+})
+
+/*
+ * 获取详情
+ * */
+router.get('/:user', async (cxt) => {
+  /*
+  * 密码隐藏不显示
+  * */
+  cxt.user.password = ''
+  cxt.body = {
+    user: cxt.user
+  }
 })
 
 
