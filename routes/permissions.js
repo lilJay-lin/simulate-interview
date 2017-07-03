6994f75f342fb0f24a657104c0d208246f0c3767
lilJay-lin <lin_xjie@foxmail.com>
2017-07-03 10:43:44 +0800

    #add 增加批量修改权限；修复查询功能

--- routes/permissions.js
+++ routes/permissions.js
@@ -52,13 +52,27 @@ router.post('/:id', async (cxt) => {
   cxt.body = {}
 })
 
+router.post('/batch/:ids', async (cxt) => {
+  const body = cxt.request.body
+  let ids = _.map((cxt.params.ids || '').split(','), (id) => {
+    return roleDao.caseObjectId(id)
+  })
+  const role = await roleDao.update({_id: {$in: ids}}, body)
+  cxt.body = {}
+})
+
 /*
 * 搜索
 * */
 router.get('/', async (cxt) => {
+  const queryParam = {}
   const name = cxt.query['name']
-  const status = cxt.query['status'] === false ? false : true
-  let list = await permissionDao.pageQuery({queryParam: {name: {$regex: decodeURIComponent(name)}}, status})
+  const status = cxt.query['status'] === false || cxt.query['status'] === 'false' ? false : true
+  queryParam.status = status
+  if (name !== undefined) {
+    queryParam.name = {$regex: decodeURIComponent(name)}
+  }
+  let list = await permissionDao.pageQuery({queryParam})
   cxt.body = list
 })
 
